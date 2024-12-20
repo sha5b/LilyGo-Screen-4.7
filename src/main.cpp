a91da667cc0ecb192037f78f8f23480e312c9bd5
sha5b <ned.tabulov@gmail.com>
2024-12-20 12:57:38 +0100

    Hello World !

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include "esp_heap_caps.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "firasans.h"
 
 // Reusable display component
 class EpaperDisplay {
@@ -44,25 +45,15 @@ public:
         epd_poweron();
         epd_clear();
 
-        // Draw a simple test pattern - black rectangle in center
-        int centerX = EPD_WIDTH / 2;
-        int centerY = EPD_HEIGHT / 2;
-        
         // Clear to white first
         memset(framebuffer, 0xFF, EPD_WIDTH * EPD_HEIGHT / 2);
-        
-        // Draw a black rectangle in center
-        for (int y = centerY - 50; y < centerY + 50; y++) {
-            for (int x = centerX - 100; x < centerX + 100; x++) {
-                // Calculate position in framebuffer
-                int pos = (y * EPD_WIDTH + x) / 2;
-                if (x % 2 == 0) {
-                    framebuffer[pos] = (framebuffer[pos] & 0x0F) | (0 << 4);
-                } else {
-                    framebuffer[pos] = (framebuffer[pos] & 0xF0) | 0;
-                }
-            }
-        }
+
+        // Set text position in center
+        int cursor_x = EPD_WIDTH/2;
+        int cursor_y = EPD_HEIGHT/2;
+
+        // Write text using write_string function
+        write_string(&FiraSans, text, &cursor_x, &cursor_y, framebuffer);
 
         // Display the framebuffer
         epd_draw_grayscale_image(epd_full_screen(), framebuffer);
