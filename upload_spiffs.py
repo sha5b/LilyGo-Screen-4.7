ad8410e3a882dd776b7d9412d0b0297175ea9f37
sha5b <ned.tabulov@gmail.com>
2024-12-20 14:09:17 +0100

    SPIFF Init

--- upload_spiffs.py
+++
@@ -1,7 +0,0 @@
-Import("env")
-
-def before_upload(source, target, env):
-    print("Uploading SPIFFS data...")
-    env.Execute("pio run --target uploadfs")
-
-env.AddPreAction("upload", before_upload)
